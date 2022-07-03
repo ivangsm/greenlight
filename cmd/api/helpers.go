9255120157baf861555dea2e92eb37a98ad11758
Iván Salazar <ivangio.salazar@gmail.com>
2022-07-03 01:52:02 -0500

    dissallow unknown fields

--- cmd/api/helpers.go
+++ cmd/api/helpers.go
@@ -45,7 +45,13 @@ func (app *application) writeJSON(w http.ResponseWriter, status int, data Envelo
 }
 
 func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst interface{}) error {
-	err := json.NewDecoder(r.Body).Decode(dst)
+	const maxBytes int64 = 1_048_576
+	r.Body = http.MaxBytesReader(w, r.Body, maxBytes)
+
+	dec := json.NewDecoder(r.Body)
+	dec.DisallowUnknownFields()
+
+	err := dec.Decode(dst)
 	if err != nil {
 		var syntaxError *json.SyntaxError
 		var unmarshalTypeError *json.UnmarshalTypeError
@@ -73,6 +79,11 @@ func (app *application) readJSON(w http.ResponseWriter, r *http.Request, dst int
 		default:
 			return err
 		}
+	}
+
+	err = dec.Decode(&struct{}{})
+	if err != io.EOF {
+		return errors.New("body must only contain a single JSON value")
 	}
 
 	return nil
